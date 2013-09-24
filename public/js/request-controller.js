6403bd8ce82a6e54b9de89afd18fe60710ea5b7f
Cory Gross <CoryG89@gmail.com>
2013-09-24 18:24:55 -0500

    Adding popovers and zipcode check to request-controller

--- public/js/request-controller.js
+++ public/js/request-controller.js
@@ -3,15 +3,44 @@
 
     var $form = $('form#request-form');
     var $email = $form.children('input#email');
-    var $emailConf = $form.children('input#email');
+    var $emailConf = $form.children('input#email-conf');
+    var $zip = $form.children('input#zip');
+
+    $email.popover({
+        title: 'Validate Email',
+        content: 'Email addresses must match.',
+        placement: 'left',
+        trigger: 'manual'
+    });
+
+    $zip.popover({
+        title: 'Validate Zip Code',
+        content: 'Currently accepted zip codes are: 36830, 36832, and 36849.',
+        placement: 'left',
+        trigger: 'manual'
+    });
 
     $form.ajaxForm({
         beforeSubmit: function () {
-            if ($email.val() === $emailConf.val()) {
-                return true;
-            } else {
-                alert('Email address fields must match.');
+            if ($email.val() !== $emailConf.val()) {
+                $email.popover('show');
+                $(document).one('focusin', function () {
+                    $email.popover('hide');
+                });
                 return false;
+            }
+
+            switch ($zip.val()) {
+                case '36830':
+                case '36832':
+                case '36849':
+                    break;
+                default:
+                    $zip.popover('show');
+                    $(document).one('focusin', function () {
+                        $zip.popover('hide');
+                    });
+                    return false;
             }
         },
 
@@ -22,7 +51,7 @@
         },
 
         error: function () {
-            window.location.replace('/volunteer/failure');
+            window.location.replace('/request/failure');
         }
     });
 
