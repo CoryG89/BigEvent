3cb3645b87d7b98284f263e13b9f6130dd1421e2
Cory Gross <CoryG89@gmail.com>
2013-11-01 22:55:36 -0500

    Finishing work on server/debug.js
    Made it so that the callback would be passed the formatted result string even if
    the debug module is disabled. This is what I meant to do last commit. I also
    added a lot of descriptive comments so its easy to tell exactly how this module
    works.

--- server/debug.js
+++ server/debug.js
@@ -4,48 +4,72 @@ var util = require('util');
 var globallyEnabled = false;
 
 module.exports = {
-    setEnabled: function (value) {
-        if (typeof value === 'boolean') globallyEnabled = value;
-    },
+    /** Returns a log function that acts as a shorthand for console.log. Each
+        returned function can be enabled/disabled separately, and each can set
+        a string which is prefixed to each string logged. Also allows a callback
+        as the last argument which is passed the resulting formatted string */
     getLogger: function (options) {
         var prefix = options.prefix !== undefined ? options.prefix : null;
         var enabled = options.enabled !== undefined ? options.enabled : true;
 
-        var Logger = function Logger () {
+        var log = function log () {
             var callback;
-            var callbackReturn = null;
-            if (globallyEnabled && enabled) {
-                var args = Array.prototype.slice.call(arguments);
-                var last = args.length - 1;
+            var formatted = null;
 
-                if (typeof args[last] === 'function')
-                    callback = args.splice(last)[0];
+            /** Get the arguments object a real array */
+            var args = Array.prototype.slice.call(arguments);
 
-                if (prefix)
-                    args[0] = prefix + args[0];
+            /** Check if last argument is a function, if so then
+                splice it off and use it as a callback */
+            var last = args.length - 1;
+            if (typeof args[last] === 'function')
+                callback = args.splice(last)[0];
 
-                console.log.apply(console, args);
-                callbackReturn = util.format.apply(util, args);
-            }
-            if (callback) callback(callbackReturn);
+            /** Prepend the prefix if set */
+            if (typeof prefix === 'string')
+                args[0] = prefix + args[0];
+
+            /** Format the string with util module and log it */
+            formatted = util.format.apply(util, args);
+
+            /** If we're not currently disabled, log it to the console */
+            if (globallyEnabled && enabled)
+                console.log(formatted);
+
+            /** If present, pass the callback the formatted string */
+            if (callback) callback(formatted);
         };
 
-        Logger.setEnabled = function (value) {
+        /** Enables/disables this particular log function */
+        log.setEnabled = function (value) {
             if (typeof value === 'boolean') enabled = value;
         };
 
-        Logger.setPrefix = function (value) {
+        /** Prefixes a string to each log for this particular log function */
+        log.setPrefix = function (value) {
             if (typeof value === 'string') prefix = value;
         };
 
-        Logger.getEnabled = function () {
+        /** Returns the status of this log particular log function */
+        log.getEnabled = function () {
             return enabled;
         };
 
-        Logger.getPrefix = function () {
+        /** Returns the prefix of this particular log function */
+        log.getPrefix = function () {
             return prefix;
         };
 
-        return Logger;
+        return log;
+    },
+    
+    /** Enables/disables all logging to the console */
+    setEnabled: function (value) {
+        if (typeof value === 'boolean') globallyEnabled = value;
+    },
+
+    /** Enables/disables all logging to the console */
+    getEnabled: function (value) {
+        if (typeof value === 'boolean') globallyEnabled = value;
     }
 };
