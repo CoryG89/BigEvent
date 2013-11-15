6a939055b6362581d0d84c4415ed03c4e9135148
Cory Gross <CoryG89@gmail.com>
2013-11-15 11:20:14 -0600

    Adding debug module and logging calls to geocoder module.

--- server/geocoder.js
+++ server/geocoder.js
@@ -3,6 +3,9 @@
 var https = require('https');
 var util = require('util');
 
+var debug = require('./debug');
+var log = debug.getLogger({ prefix: '[geocoder]-  ' });
+
 var baseUri = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 module.exports = {
@@ -10,15 +13,18 @@ module.exports = {
         var encoded = encodeURIComponent(addr);
         var uri = util.format('%s?address=%s&sensor=false', baseUri, encoded);
 
+        log('Sending request to geocoding server');
         https.get(uri, function (res) {
             var data = '';
             res.on('data', function (chunk) {
                 data += chunk;
             });
             res.on('end', function () {
+                log('Got response from geocoding server');
                 callback(JSON.parse(data));
             });
         }).on('error', function (err) {
+            log('Error sending geocoding request:\n\n\t%s\n', err);
             callback(null, err);
         });
     }
